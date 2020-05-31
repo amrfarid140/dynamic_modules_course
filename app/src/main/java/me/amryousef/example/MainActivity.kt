7f7310ff4bfe9c748417e1cd96b54d1324060124
Amr Yousef <amr.farid140@gmail.com>
2020-05-31 12:55:20 +0100

    Prepare for play store submission

--- app/src/main/java/me/amryousef/example/MainActivity.kt
+++ app/src/main/java/me/amryousef/example/MainActivity.kt
@@ -7,15 +7,18 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.play.core.splitcompat.SplitCompat
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 import kotlinx.android.synthetic.main.activity_main.*
-
-//import me.amryousef.small_dynamic_module.SecondActivity
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         const val REQUEST_CODE = 1
     }
+
+    private lateinit var splitInstallManager: SplitInstallManager
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(newBase)
@@ -26,12 +29,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "Main Activity"
+        splitInstallManager = SplitInstallManagerFactory.create(applicationContext)
         main_to_activity2_button.setOnClickListener {
-//            val intent = Intent(this, SecondActivity::class.java)
-//            startActivityForResult(
-//                intent,
-//                REQUEST_CODE
-//            )
+            splitInstallManager.startInstall(
+                SplitInstallRequest.newBuilder()
+                    .addModule("small_dynamic_module")
+                    .build()
+            ).addOnSuccessListener {
+                getClassForActivity()?.let { activityClass ->
+                    val intent = Intent(this, activityClass)
+                    startActivityForResult(
+                        intent,
+                        REQUEST_CODE
+                    )
+                } ?: Toast.makeText(this, "Unable to load class", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Unable to install module", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getClassForActivity(): Class<*>? {
+        return try {
+            Class.forName("me.amryousef.small_dynamic_module.SecondActivity")
+        } catch (exception: ClassNotFoundException) {
+            null
         }
     }
 
@@ -39,6 +61,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             Toast.makeText(this, "2nd Activity finished", Toast.LENGTH_SHORT).show()
+            splitInstallManager.deferredUninstall(listOf("small_dynamic_module"))
         }
     }
 }
